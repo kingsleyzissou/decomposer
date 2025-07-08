c5c18a2c752f1670ed8f8474f2926679c1bdaa64
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-08 10:07:47 +0100

    api: create a module for meta endpoints
    This organises the code into modules for the various endpoints.
    Logically, it keeps all the code for the module together making
    it easier to work with the codebase.

---
+++ src/api/README.md
@@ -0,0 +1,4 @@
+# Modules directory
+
+We're building this directory up incrementally, but it makes sense to keep specific
+modules together, so it's easier to track the project and what each item does.
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 
-import { SOCKET_PATH } from '@app/constants';
-import { router } from '@app/router';
+import * as api from '@app/api';
+import { API_ENDPOINT, SOCKET_PATH } from '@app/constants';
+import { notFound } from '@app/middleware';
 
 // we need to make sure that the socket doesn't
 // already exist, otherwise we run into issues
@@ -14,7 +15,7 @@ if (await Bun.file(SOCKET_PATH).exists()) {
 }
 
 const app = new Hono();
-app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404));
+app.notFound(notFound);
 
 // these middlewares need to be registered
 // on the app-level (or main router), since
@@ -24,8 +25,8 @@ const middleware = new Hono();
 middleware.use(prettyJSON());
 middleware.use(logger());
 
-app.route('/', middleware);
-app.route('/', router);
+app.route(API_ENDPOINT, middleware);
+app.route(API_ENDPOINT, api.meta);
 
 const server = {
   fetch: app.fetch,
