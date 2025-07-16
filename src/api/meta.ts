53740fb4efc74f40120e8b62f1f5d22090bd0538
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-16 09:30:01 +0100

    middleware: move openapi schema to meta routes
    Only the meta routes need access to the schema, so let's
    just mount it there. Additionally, fix the logic so that
    the schema file isn't loaded on every request.

--- src/api/meta.ts
+++ src/api/meta.ts
@@ -1,7 +1,27 @@
 import { Context } from 'hono';
 import { Hono } from 'hono';
 
-const meta = new Hono()
+import { SCHEMA_PATH } from '@app/constants';
+
+// load the openapi spec on app startup
+// rather than inside the middleware function,
+// otherwise the file is loaded on every request.
+const schema = await Bun.file(SCHEMA_PATH).json();
+
+type RouteContext = {
+  Variables: {
+    schema: JSON;
+  };
+};
+
+const meta = new Hono<RouteContext>()
+
+  // the meta routes are the only routes that
+  // need access to the openapi spec
+  .use(async (ctx, next) => {
+    ctx.set('schema', schema);
+    await next();
+  })
 
   .get('/ready', (ctx: Context) => {
     return ctx.json({ readiness: 'ready' });
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export type AppContext = {
-  Variables: {
-    schema: JSON;
-  };
-};
