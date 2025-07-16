17bb5e252833bea05527c01977f36f0b59a14d78
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-16 09:30:01 +0100

    middleware: move openapi schema to meta routes
    Only the meta routes need access to the schema, so let's
    just mount it there. Additionally, fix the logic so that
    the schema file isn't loaded on every request.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import chalk from 'chalk';
 import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { prettyJSON } from 'hono/pretty-json';
 
 import * as api from '@app/api';
 import { API_ENDPOINT, SOCKET_PATH } from '@app/constants';
 import { notFound } from '@app/errors';
-import { middleware } from '@app/middleware';
 
 // we need to make sure that the socket doesn't
 // already exist, otherwise we run into issues
@@ -15,6 +16,10 @@ if (await Bun.file(SOCKET_PATH).exists()) {
 
 const app = new Hono();
 app.notFound(notFound);
+
+export const middleware = new Hono();
+middleware.use(prettyJSON());
+middleware.use(logger());
 
 app.route(API_ENDPOINT, middleware);
 app.route(API_ENDPOINT, api.meta);
