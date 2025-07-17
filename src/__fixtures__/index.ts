f486dcf84aab1e14a9ea3bf503291b1a998f9d37
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-17 17:09:05 +0100

    blueprint: add a blueprint conversion tool
    We need to convert hosted customizations to on-prem blueprints. To do
    this, I copied the existing conversion tool from the frontend & modified
    it slightly. Additionally, we can wrap this conversion in a validator,
    thanks to our zod schemas, to ensure that the conversion was okay.

--- src/__fixtures__/index.ts
+++ src/__fixtures__/index.ts
@@ -1,1 +1,8 @@
+import path from 'node:path';
+
+export { customizations, name } from './customizations';
 export { composeRequest } from './compose-request';
+
+export const blueprint = await Bun.file(
+  path.join(__dirname, 'blueprint.json'),
+).json();
