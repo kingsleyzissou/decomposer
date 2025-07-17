dd3c049c1bae9f7552a1ae2dbc5b748162643b40
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-17 17:09:05 +0100

    blueprint: add a blueprint conversion tool
    We need to convert hosted customizations to on-prem blueprints. To do
    this, I copied the existing conversion tool from the frontend & modified
    it slightly. Additionally, we can wrap this conversion in a validator,
    thanks to our zod schemas, to ensure that the conversion was okay.

--- src/queue/processor.ts
+++ src/queue/processor.ts
@@ -1,16 +1,36 @@
 import { mkdir } from 'fs/promises';
 import path from 'path';
+import z from 'zod';
 
+import { mapHostedToOnPrem } from '@app/blueprint';
 import { logger } from '@app/logger';
 import { ComposeRequest } from '@app/types';
 import { jsonFormat } from '@app/utilities';
+import { Customizations } from '@gen/ibcrc/zod';
 
 import { Job } from './types';
+
+type Customizations = z.infer<typeof Customizations>;
 
 const saveRequest = async (outputDir: string, request: ComposeRequest) => {
   await Bun.file(path.join(outputDir, 'request.json')).write(
     jsonFormat(request),
   );
+};
+
+const saveBlueprint = async (
+  outputDir: string,
+  id: string,
+  customizations?: Customizations,
+) => {
+  const blueprint = mapHostedToOnPrem({
+    name: id,
+    customizations: customizations || {},
+  });
+
+  const bpPath = path.join(outputDir, 'blueprint.json');
+  await Bun.file(bpPath).write(jsonFormat(blueprint));
+  return bpPath;
 };
 
 const handleExit = (id: string, outputDir: string) => {
@@ -33,6 +53,7 @@ export const buildImage = (
     const outputDir = path.join(store, id);
     await mkdir(outputDir, { recursive: true });
     await saveRequest(outputDir, request);
+    const bpPath = await saveBlueprint(outputDir, id, request.customizations);
 
     // there should only be one item, we have already validated this
     const imageType = request.image_requests[0].image_type;
@@ -41,6 +62,8 @@ export const buildImage = (
       cmd: [
         executable,
         'build',
+        '--blueprint',
+        bpPath,
         '--output-dir',
         outputDir,
         '--distro',
