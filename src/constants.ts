dc3d9228b5e96c41a1f91cc49e080267d8157483
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-18 12:20:24 +0100

    constants: fix the socket path

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const API_ENDPOINT = '/api/image-builder-composer/v2';
-export const SOCKET_PATH = '/tmp/decomposer-httpd.sock';
+export const SOCKET_PATH = '/run/decomposer-httpd.sock';
 
 // Bun.file() imports need to be relative to the project root
 export const SCHEMA_PATH = './generated/ibcrc/v1/api.json';
