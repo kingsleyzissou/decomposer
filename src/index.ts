1c1a13a206515de26de9304f7b6a0415a2e5aba6
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-18 11:09:09 +0100

    main: add graceful shutdown
    Gracefully shutdown the application and remove the socket.

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,12 @@ import { cliArgs } from '@app/cli';
 import { API_ENDPOINT, SOCKET_PATH } from '@app/constants';
 import { notFound } from '@app/errors';
 import { logger } from '@app/logger';
-import { prettyPrint } from '@app/utilities';
+import { prettyPrint, removeSocket } from '@app/utilities';
 
 // we need to make sure that the socket doesn't
 // already exist, otherwise we run into issues
 // where the server can't run
-if (await Bun.file(SOCKET_PATH).exists()) {
-  await Bun.file(SOCKET_PATH).unlink();
-}
+await removeSocket();
 
 const app = new Hono();
 app.notFound(notFound);
@@ -38,7 +36,14 @@ Bun.serve(server);
 // ping the socket as a non-privileged user
 await chmod(SOCKET_PATH, 0o775);
 
-// TODO: listen for app shutdown and remove the socket
+const shutdown = async (signal: string) => {
+  logger.info(`Received ${signal}, shutting down gracefully...`);
+  await removeSocket();
+  process.exit(0);
+};
+
+process.on('SIGINT', () => shutdown('SIGINT'));
+process.on('SIGTERM', () => shutdown('SIGTERM'));
 
 logger.info(
   `🚀 Decomposer server started with configuration:\n${prettyPrint({
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,3 +1,11 @@
+import { SOCKET_PATH } from '@app/constants';
+
+export const removeSocket = async () => {
+  if (await Bun.file(SOCKET_PATH).exists()) {
+    await Bun.file(SOCKET_PATH).unlink();
+  }
+};
+
 export const prettyPrint = (o: object) => {
   return Object.entries(o)
     .map(([key, value]) => {
