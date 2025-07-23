112fb4ca92686094e794af9c7154f416655a4b24
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-23 16:38:11 +0100

    utilities: add transaction wrapper function
    Add a higher order function that exectues store operations. If any
    errors occur, throw a new custom Database error that the application
    knows how to handle.

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,4 +1,5 @@
 import { SOCKET_PATH } from '@app/constants';
+import { AppError, DatabaseError, isPouchError } from '@app/errors';
 
 export const removeSocket = async () => {
   if (await Bun.file(SOCKET_PATH).exists()) {
@@ -16,4 +17,20 @@ export const prettyPrint = (o: object) => {
 
 export const jsonFormat = (o: object) => {
   return JSON.stringify(o, null, 2);
+};
+
+export const withTransaction = async <T>(
+  operation: () => Promise<T>,
+): Promise<T> => {
+  try {
+    return await operation();
+  } catch (error) {
+    if (isPouchError(error)) {
+      throw new DatabaseError(error);
+    }
+    throw new AppError({
+      message: 'Unable to complete transaction',
+      details: [error],
+    });
+  }
 };
