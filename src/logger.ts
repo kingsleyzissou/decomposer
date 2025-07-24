cb28c73157df5c1d94111847225f9ffae865ca36
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-07-24 10:22:51 +0100

    logger: fix levels for testing
    Just set the level to silent for testing since the outputs can
    be quite noisy.

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,23 @@
 import { pino } from 'pino';
 
+const level = () => {
+  const env = process.env.NODE_ENV;
+  if (env === 'production') {
+    return 'info';
+  }
+
+  if (env === 'test') {
+    // debug messages make the testing suite
+    // noisier than it needs to be
+    return 'silent';
+  }
+
+  return 'trace';
+};
+
 export const logger = pino({
   base: null,
-  level: 'trace',
+  level: level(),
   transport: {
     target: 'hono-pino/debug-log',
   },
