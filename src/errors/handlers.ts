a26c56f84c9881fa700d990b9c6f08923f6a2b0c
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-07-28 13:07:22 +0000

    errors: split into a module
    The errors file was getting quite large and hard to manage. This commit
    splits the different error types into their own files inside the
    `errors` module.

---
+++ src/errors/handlers.ts
@@ -0,0 +1,31 @@
+import { ErrorHandler, NotFoundHandler } from 'hono';
+import { StatusCodes } from 'http-status-codes';
+
+import { AppError, DatabaseError, ValidationError } from '.';
+
+export const notFound: NotFoundHandler = (ctx) => {
+  throw new AppError({
+    code: StatusCodes.NOT_FOUND,
+    message: 'Path not found!',
+    details: [`The path '${ctx.req.path}' does not exist`],
+  });
+};
+
+export const onError: ErrorHandler = (error, ctx) => {
+  if (
+    error instanceof ValidationError ||
+    error instanceof AppError ||
+    error instanceof DatabaseError
+  ) {
+    const { body, code } = error.response();
+    return ctx.json(body, code);
+  }
+
+  return ctx.json(
+    {
+      code: StatusCodes.INTERNAL_SERVER_ERROR,
+      message: error.message,
+    },
+    StatusCodes.INTERNAL_SERVER_ERROR,
+  );
+};
