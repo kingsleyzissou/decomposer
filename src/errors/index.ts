e795a8bae02359a5d65a58bb31981ae710c48569
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-07-28 13:07:22 +0000

    errors: split into a module
    The errors file was getting quite large and hard to manage. This commit
    splits the different error types into their own files inside the
    `errors` module.

---
+++ src/errors/index.ts
@@ -0,0 +1,4 @@
+export { AppError } from './app';
+export { DatabaseError, withDatabaseError } from './database';
+export { ValidationError } from './validation';
+export { onError, notFound } from './handlers';
