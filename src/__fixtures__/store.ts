525e273a91ca0fb4b3b5e994ddc7a0d2d3993eaa
Gianluca Zuccarelli <gzuccare@redhat.com>
2025-08-01 15:05:49 +0000

    types: tidy these up a bit
    Make them slightly more re-usable so that we can share some of the
    definitons with the blueprint types too.

--- src/__fixtures__/store.ts
+++ src/__fixtures__/store.ts
@@ -1,14 +1,18 @@
 import pouchdb from 'pouchdb';
 import memoryAdapter from 'pouchdb-adapter-memory';
 
-import { ComposeDoc } from '@app/store';
+import { ComposeDocument } from '@app/store';
 
 pouchdb.plugin(memoryAdapter);
 
 export const createTestStore = (store: string) => {
-  const composesStore: PouchDB.Database<ComposeDoc> = new pouchdb('composes', {
-    adapter: 'memory',
-  });
+  const composesStore: PouchDB.Database<ComposeDocument> = new pouchdb(
+    'composes',
+    {
+      adapter: 'memory',
+    },
+  );
+
   return {
     path: store,
     composes: composesStore,
--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,28 @@
 import path from 'path';
 import pouchdb from 'pouchdb';
 
-import { ComposeRequest } from '@app/api/composes';
-import { Status } from '@app/constants';
+import { ComposeWithBuildStatus } from '@app/api/composes';
 
-export type ComposeDoc = {
+type Document = {
   _id: string;
   _rev?: string;
-  created_at: string;
-  status: Status;
-  request?: ComposeRequest;
 };
+
+// pouchdb uses `_id` instead of `id` for the primary key
+// we also want to keep track of the compose status in the document,
+// so we add that type too
+export type ComposeDocument = Document & ComposeWithBuildStatus;
 
 export type Store = {
   path: string;
-  composes: PouchDB.Database<ComposeDoc>;
+  composes: PouchDB.Database<ComposeDocument>;
 };
 
 export const createStore = (store: string) => {
   const composesPath = path.join(store, 'composes');
-  const composesStore: PouchDB.Database<ComposeDoc> = new pouchdb(composesPath);
+  const composesStore: PouchDB.Database<ComposeDocument> = new pouchdb(
+    composesPath,
+  );
   return {
     path: store,
     composes: composesStore,
