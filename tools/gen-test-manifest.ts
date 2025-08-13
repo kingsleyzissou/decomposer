61912c545ed4124e2a6d7435f4e660a9625c85dc
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-08-13 11:40:27 +0100

    ci: add a scheduled action for generated code
    - scheduled action to generate test manifests & API generated code
    - update the api generated code
    - update the gen-test-manifest to build manifests for both arches
    - update the test manifests

--- tools/gen-test-manifest.ts
+++ tools/gen-test-manifest.ts
@@ -1,23 +1,31 @@
 import { $ } from 'bun';
 import path from 'node:path';
 
-import { getHostArch, jsonFormat } from '../src/utilities';
+import { Architecture } from '../src/constants';
+import { jsonFormat } from '../src/utilities';
 
-const manifest = await $`image-builder manifest \
+const arches = [Architecture.X86_64, Architecture.AARCH64];
+
+const genManifest = async (arch: string) =>
+  await $`image-builder manifest \
   --distro centos-9 \
+  --arch ${arch} \
   --seed 42 \
   qcow2
 `.json();
 
-const hostArch = getHostArch();
+for await (const arch of arches) {
+  console.log(`📝 Generating manifest for ${arch}`);
+  const manifest = await genManifest(arch);
 
-await Bun.file(
-  path.join(
-    __dirname,
-    '..',
-    'integration',
-    '__fixtures__',
-    hostArch,
-    'centos-9.qcow2.json',
-  ),
-).write(jsonFormat(manifest));
+  await Bun.file(
+    path.join(
+      __dirname,
+      '..',
+      'integration',
+      '__fixtures__',
+      arch,
+      'centos-9.qcow2.json',
+    ),
+  ).write(jsonFormat(manifest));
+}
