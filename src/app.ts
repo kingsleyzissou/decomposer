a897aa7f5b8b850213820a4ec91cb43b21e7b68b
Gianluca Zuccarelli <gianlucazuccarelli@gmail.com>
2025-08-28 13:24:49 +0100

    errors: add custom errors and error handlers
    Add an errors module that contains some custom error types that can
    be used to handle errors inside the app. Additionally, add some error
    handlers and register them with the router.

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { prettyJSON } from 'hono/pretty-json';
 
+import { notFound, onError } from '@app/errors';
+
 const middleware = new Hono();
 middleware.use(prettyJSON());
 middleware.use(logger());
 
 // chaining the functions helps preserve RPC type inference
 export const app = new Hono()
-  .notFound((c) => c.json({ message: 'Not Found', ok: false }, 404))
+  .notFound(notFound)
+  .onError(onError)
   .route('*', middleware)
   .get('/health', (c) => c.json({ message: 'OK', ok: true }, 200));
